9df542cb104b1af23cf416f13c81c928f1d3ba3a
buenaSopa <bryankoh17102@gmail.com>
2022-10-27 08:20:58 +0800

    added network script.sh

---
+++ network.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+stty -echoctl
+
+trap "ctrl_c" SIGINT
+
+ctrl_c() {
+    echo "shutting down network..."
+}
+
+docker rm trading-platform_kafka_1 trading-platform_zookeeper_1
+export PRI_IP=$(hostname -I | awk '{print $1}')
+
+docker-compose up
+
